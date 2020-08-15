289a489d8e6a33510266ac7e0ca78cd609035d88
Samuel Michael Squire <sam@samsquire.com>
2020-08-15 17:07:59 +0100

    Sam initial commit

---
+++ hash-db.py
@@ -0,0 +1,52 @@
+import pygtrie
+
+class Db():
+    def __init__(self):
+        self.db = {}
+        self.sort_index = pygtrie.CharTrie()
+
+    def store(self, partition_key, sort_key, item):
+        self.db[partition_key + ":" + sort_key] = item
+        self.sort_index[partition_key + ":" + sort_key] = sort_key
+        if sort_key not in self.sort_index:
+            self.sort_index[sort_key] = pygtrie.CharTrie()
+        self.sort_index[sort_key][partition_key] = partition_key + ":" + sort_key
+
+    def query_begins(self, partition_key, query):
+        for sort_key, lookup_key in self.sort_index.items(prefix=partition_key + ":" + query):
+            yield (lookup_key, self.db[sort_key])
+
+    def query_pk_begins(self, partition_key, query):
+        for sort_key, value in self.sort_index.items(prefix=partition_key):
+            for partition_key, value in value.items(prefix=query):
+                yield (partition_key, self.db[value])
+
+    def query_between(self, partition_key, from_query, to_query):
+        stop_sort_key, stop_lookup_key = next(self.sort_index.iteritems(prefix=partition_key + ":" + to_query))
+        for sort_key, lookup_key in self.sort_index.items(prefix=partition_key + ":" + from_query):
+            yield (lookup_key, self.db[sort_key])
+            if sort_key == stop_sort_key:
+                break
+        for sort_key, lookup_key in self.sort_index.items(prefix=partition_key + ":" + to_query):
+            yield (lookup_key, self.db[sort_key])
+
+
+
+
+
+db = Db()
+db.store("user#samsquire", "following#dinar", ["Messages 1"])
+db.store("user#samsquire", "following#someonelse", ["Messages 2"])
+db.store("user#samsquire", "message#2020-08-01T14:39", ["Messages 1"])
+db.store("user#samsquire", "message#2020-07-01T14:39", ["Messages 2"])
+db.store("user#samsquire", "message#2020-06-01T09:30", ["Messages 3"])
+db.store("user#samsquire", "message#2020-06-01T14:39", ["Messages 4"])
+db.store("user#dinar", "message#2020-06-01T14:39", ["Messages 5"])
+
+# find followers of user user#samsquire
+print(list(db.query_begins("user#samsquire", "following")))
+# find messages by samsquire between dates
+print(list(db.query_between("user#samsquire", "message#2020-06-01", "message#2020-07-01")))
+
+print("find all users who sent messages")
+print(list(db.query_pk_begins("message", "user")))
