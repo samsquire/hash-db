efbf7459df22a0ab5c3aaa00d9306abbd16de3ef
Samuel Michael Squire <sam@samsquire.com>
2020-08-23 21:08:28 +0100

    Sam sorting

--- hash-db.py
+++ hash-db.py
@@ -12,20 +12,27 @@ class Db():
             self.sort_index[sort_key] = pygtrie.CharTrie()
         self.sort_index[sort_key][partition_key] = partition_key + ":" + sort_key
 
-    def query_begins(self, partition_key, query):
-        for sort_key, lookup_key in self.sort_index.items(prefix=partition_key + ":" + query):
-            yield (lookup_key, self.db[sort_key])
+    def query_begins(self, partition_key, query, sortmode):
+        def items():
+            for sort_key, lookup_key in self.sort_index.iteritems(prefix=partition_key + ":" + query):
+                yield (sort_key, lookup_key, self.db[sort_key])
+        return sorted(items(), key=lambda x: x[0], reverse=sortmode == "desc")
 
-    def query_pk_begins(self, partition_key, query):
-        for sort_key, value in self.sort_index.items(prefix=partition_key):
-            for partition_key, value in value.items(prefix=query):
-                yield (partition_key, self.db[value])
 
-    def query_between(self, partition_key, from_query, to_query):
-        for sort_key, lookup_key in self.sort_index.items(prefix=partition_key + ":" + from_query):
-            yield (lookup_key, self.db[sort_key])
-        for sort_key, lookup_key in self.sort_index.items(prefix=partition_key + ":" + to_query):
-            yield (lookup_key, self.db[sort_key])
+    def query_pk_begins(self, partition_key, query, sortmode):
+        def items(partition_key, query):
+            for sort_key, value in self.sort_index.iteritems(prefix=partition_key):
+                for partition_key, value in value.iteritems(prefix=query):
+                    yield (sort_key, partition_key, self.db[value])
+        return sorted(items(partition_key, query), key=lambda x: x[0], reverse=sortmode == "desc")
+
+    def query_between(self, partition_key, from_query, to_query, sortmode):
+        def items():
+            for sort_key, lookup_key in self.sort_index.items(prefix=partition_key + ":" + from_query):
+                yield (sort_key, lookup_key, self.db[sort_key])
+            for sort_key, lookup_key in self.sort_index.items(prefix=partition_key + ":" + to_query):
+                yield (sort_key, lookup_key, self.db[sort_key])
+        return sorted(items(), key=lambda x: x[0], reverse=sortmode == "desc")
 
 
 
@@ -34,18 +41,18 @@ class Db():
 db = Db()
 db.store("user#samsquire", "following#dinar", ["Messages 1"])
 db.store("user#samsquire", "following#someonelse", ["Messages 2"])
-db.store("user#samsquire", "message#2020-08-01T14:39", ["Messages 1"])
+db.store("user#samsquire", "message#2020-05-01T14:39", ["Messages 1"])
 db.store("user#samsquire", "profile", ["profile"])
-db.store("user#samsquire", "message#2020-07-01T14:39", ["Messages 2"])
-db.store("user#samsquire", "message#2020-06-01T09:30", ["Messages 3"])
-db.store("user#samsquire", "message#2020-06-01T14:39", ["Messages 4"])
-db.store("user#dinar", "message#2020-06-01T14:39", ["Messages 5"])
+db.store("user#samsquire", "message#2020-06-01T14:39", ["Messages 2"])
+db.store("user#samsquire", "message#2020-07-01T09:30", ["Messages 3"])
+db.store("user#samsquire", "message#2020-08-01T14:39", ["Messages 4"])
+db.store("user#dinar", "message#2020-09-01T14:39", ["Messages 5"])
 
 print("find followers of user user#samsquire")
-print(list(db.query_begins("user#samsquire", "following")))
+print(list(db.query_begins("user#samsquire", "following", sortmode="asc")))
 
 print("find messages by samsquire between dates")
-print(list(db.query_between("user#samsquire", "message#2020-06-01", "message#2020-07-01")))
+print(list(db.query_between("user#samsquire", "message#2020-06-01", "message#2020-07-01", sortmode="asc")))
 
 print("find all users who sent messages")
-print(list(db.query_pk_begins("message", "user")))
+print(list(db.query_pk_begins("message", "user", sortmode="desc")))
