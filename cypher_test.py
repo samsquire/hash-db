279b2dda5949e33c5fa8759dc436595d449cddae
Farley Knight <farleyknight@gmail.com>
2022-05-18 14:26:13 -0400

    Add upper case version of the test.

--- cypher_test.py
+++ cypher_test.py
@@ -21,3 +21,25 @@ def test_parse_match():
 
     assert parser.statement == match_query
     assert parser.graph == expected_graph
+
+def test_parse_match_case_insensitive():
+    match_query = "MATCH (actor)-[:ACTED_IN]->(wallstreet:Movie {title: 'Wall Street'}) RETURN actor"
+    parser = CypherParser()
+    parser.parse(match_query)
+
+    expected_graph = [
+        {'kind': 'match',
+         'variable': 'actor'
+        },
+        {'kind': 'relationship',
+         'name': 'ACTED_IN'
+        },
+        {'attributes': {'title': 'Wall Street'},
+         'kind': 'match',
+         'label': 'Movie',
+         'variable': 'wallstreet'
+        }
+    ]
+
+    assert parser.statement == match_query
+    assert parser.graph == expected_graph
