c047156b9dee60620962081ba8093400840c7f58
Farley Knight <farleyknight@gmail.com>
2022-05-18 12:36:45 -0400

    Update Cypher query

--- cypher_test.py
+++ cypher_test.py
@@ -1,15 +1,8 @@
 from cypher import CypherParser
 
 match_query = """
-MATCH (wallstreet:Movie {title: 'Wall Street'})<-[:ACTED_IN]-(actor)
-RETURN actor.name
+MATCH (actor)-[:ACTED_IN]->(wallstreet:Movie {title: 'Wall Street'}) RETURN actor
 """
-
-def test_parse_gettok():
-    parser = CypherParser()
-    token = parser.gettok()
-
-    assert token == None
 
 def test_parse_match():
     parser = CypherParser()
