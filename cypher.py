1671c86e0c982d5e425c59a021fb2b5e6c3b2d7b
Samuel Squire <sam@samsquire.com>
2022-05-18 18:03:58 +0100

    Sam lowercase identifiers

--- cypher.py
+++ cypher.py
@@ -64,7 +64,7 @@ class CypherParser():
             if self.end and self.last_char != ")":
                 identifier += self.last_char
             
-            return identifier
+            return identifier.lower()
     
         if self.last_char == "=":
             self.last_char = self.getchar()
